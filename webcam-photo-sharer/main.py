6a618bdb3d5ea08d59f5569bcfb292bd761afafa
Sashir Estela <sashirestela@gmail.com>
2024-08-28 02:52:31 +0000

    Webcam-Photo-Sharer project

---
+++ webcam-photo-sharer/main.py
@@ -0,0 +1,38 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from filesharer import FileSharer
+
+Builder.load_file('./frontend.kv')
+
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play = True
+        self.ids.camera_button.text = 'Stop Camera'
+        self.ids.camera.texture = self.ids.camera._camera.texture
+
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.camera_button.text = 'Start Camera'
+        self.ids.camera.texture = None
+
+    def capture(self):
+        pass
+
+
+class ImageScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+mainApp = MainApp()
+mainApp.run()
